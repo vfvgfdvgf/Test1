6c8f2bcd6fe8ee960d7388e203481a7dda09e2fa
vfvgfdvgf <halax.7y7@gmail.com>
2025-11-05 21:46:42 +0300

    تحديث settings.py وإضافة staticfiles

--- thaqaf_arabi/settings.py
+++ thaqaf_arabi/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 # ==============================
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # ✅ أضف هذا السطر
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
